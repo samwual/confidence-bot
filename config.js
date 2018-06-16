1dc8bcb25d136290dc8a4e51bb219b0b63a10221
Sam Wu <40319887+samwual@users.noreply.github.com>
2018-06-16 15:31:08 -0400

    Removing config.js file

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	consumer_key:         'TEST',
-  	consumer_secret:      'TEST',
-  	access_token:         'TEST',
- 	access_token_secret:  'TEST',
-  	timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-  	strictSSL:            true,     // optional - requires SSL certificates to be valid.
-}
